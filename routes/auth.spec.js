a40e87199f2fb4b66d3f337ad45275ae3bd4f3c1
nomadic4life <orbison.dennis@gmail.com>
2020-01-01 11:34:56 -0800

    updated test for login route with mocks of the models

--- routes/auth.spec.js
+++ routes/auth.spec.js
@@ -1,95 +1,133 @@
 require("dotenv").config();
 const request = require("supertest");
 const server = require("../api-server");
-const model = "fake" || require("./mockModel");
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET || "temp";
-jest.mock("../data/db");
 
-// describe("POST /api/auth/login", () => {
-//   it("should return 200 http status code with Token.", async () => {
-//     const username = "username",
-//       passord = "password";
-//     const response = await request(server(model))
-//       .post("/api/auth/login")
-//       .auth(username, passord);
+jest.mock("../models");
 
-//     const { message } = response.body;
+describe("\nPOST /api/auth/login", () => {
+  it("01 - Should return 200 http status code with Token.", async () => {
+    const username = "testuser",
+      password = "TestPass";
+    const response = await request(server)
+      .post("/api/auth/login")
+      .auth(username, password);
 
-//     const payload = {
-//       id: 0,
-//       username
-//     };
+    const { message } = response.body;
 
-//     expect(message).toBe(jwt.sign(payload, secret));
-//     expect(response.status).toBe(200);
-//   });
+    const payload = {
+      id: 1,
+      username
+    };
 
-//   it("should return 401 for missing passord with message", () => {
-//     const username = "username",
-//       passord = "";
-//     return request(server("this should be a model"))
-//       .post("/api/auth/login")
-//       .auth(username, passord)
-//       .then(response => {
-//         const { message } = response.body;
-//         expect(message).toBe("Missing Credentials");
-//         expect(response.status).toBe(401);
-//       });
-//   });
+    expect(message).toBe(jwt.sign(payload, secret));
+    expect(response.status).toBe(200);
+  });
 
-// it("should return 401 for missing username with message", () => {
-//   const username = "",
-//     passord = "password";
-//   return request(server("this should be a model"))
-//     .post("/api/auth/login")
-//     .auth(username, passord)
-//     .then(response => {
-//       const { message } = response.body;
-//       expect(message).toBe("Missing Credentials");
-//       expect(response.status).toBe(401);
-//     });
-// });
+  it("02 - Should return 500 HTTP status code for server error.", () => {
+    const username = "error",
+      password = "TestPass";
+    return request(server)
+      .post("/api/auth/login")
+      .auth(username, password)
+      .then(response => {
+        const { message } = response.body;
+        expect(message).toBe("Server Error.");
+        expect(response.status).toBe(500);
+      });
+  });
 
-// it("should return 401 for missing username and password with message", () => {
-//   const username = "",
-//     passord = "";
-//   return request(server("this should be a model"))
-//     .post("/api/auth/login")
-//     .auth(username, passord)
-//     .then(response => {
-//       const { message } = response.body;
-//       expect(message).toBe("Missing Credentials");
-//       expect(response.status).toBe(401);
-//     });
-// });
+  describe("  03 - Should return 401 HTTP status code for", () => {
+    it("03.1 Empty Authorization in header.", () => {
+      return request(server)
+        .post("/api/auth/login")
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Username and Password required.");
+          expect(response.status).toBe(401);
+        });
+    });
 
-// it("should return 401 for wrong Auth type", () => {
-//   const username = "username",
-//     passord = "password";
-//   return request(server("this should be a model"))
-//     .post("/api/auth/login")
-//     .auth(username, passord, { type: "bearer" })
-//     .then(response => {
-//       const { message } = response.body;
-//       expect(message).toBe("Not Basic Auth Type.");
-//       expect(response.status).toBe(401);
-//     });
-// });
+    it("03.2 Wrong Authorization type.", () => {
+      const username = "username",
+        password = "password";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password, { type: "bearer" })
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Basic Auth Type is required.");
+          expect(response.status).toBe(401);
+        });
+    });
 
-// it("should return 401 http status code for incorrect username or password.", () => {
-//   const username = "wrongusername",
-//     passord = "wrongpassword";
-//   return request(server(model))
-//     .post("/api/auth/login")
-//     .auth(username, passord)
-//     .then(response => {
-//       const { message } = response.body;
-//       expect(message).toBe("Incorrect Credentials");
-//       expect(response.status).toBe(401);
-//     });
-// });
-// });
+    it("03.3 Missing password.", () => {
+      const username = "username",
+        password = "";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password)
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Missing Credentials.");
+          expect(response.status).toBe(401);
+        });
+    });
+
+    it("03.4 Missing username.", () => {
+      const username = "",
+        password = "password";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password)
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Missing Credentials.");
+          expect(response.status).toBe(401);
+        });
+    });
+
+    it("03.5 Missing username and password.", () => {
+      const username = "",
+        password = "";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password)
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Missing Credentials.");
+          expect(response.status).toBe(401);
+        });
+    });
+
+    it("03.6 Username not found.", () => {
+      const username = "wrongusername",
+        password = "TestPass";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password)
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Invalid Credentials.");
+          expect(response.status).toBe(401);
+        });
+    });
+
+    it("03.7 Incorrect password.", () => {
+      const username = "testuser",
+        password = "password";
+      return request(server)
+        .post("/api/auth/login")
+        .auth(username, password)
+        .then(response => {
+          const { message } = response.body;
+          expect(message).toBe("Invalid Credentials.");
+          expect(response.status).toBe(401);
+        });
+    });
+  });
+});
 
 // describe("POST /api/auth/signup", () => {
 //   it("should return 201 http status code with Token.", () => {
@@ -177,17 +215,17 @@ jest.mock("../data/db");
 //   });
 // });
 
-describe("testing testing...", () => {
-  it("should test the test route to test mocking of models", async () => {
-    const username = "testuser",
-      password = "TestPass";
+// describe("testing testing...", () => {
+//   it("should test the test route to test mocking of models", async () => {
+//     const username = "testuser",
+//       password = "TestPass";
 
-    const response = await request(server(model))
-      .post("/api/test")
-      .send({ username, password });
+//     const response = await request(server)
+//       .post("/api/test")
+//       .send({ username, password });
 
-    const { message } = response.body;
+//     const { message } = response.body;
 
-    expect(response.status).toBe(200);
-  });
-});
+//     expect(response.status).toBe(200);
+//   });
+// });
