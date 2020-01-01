e1e6f60e6e7bda5e8c78b57d17965f926b543c25
nomadic4life <orbison.dennis@gmail.com>
2020-01-01 12:04:21 -0800

    created new middleware for auth checking and validation

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,21 @@
+const { userModel } = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const secret = process.env.JWT_SECRET || "temp";
+const StatusError = require("../utils/errors");
+
 const validAuth = async (req, res, next) => {
   const { authorization } = req.headers;
-  const unauthorized = 401;
+
+  if (!authorization) {
+    throw new StatusError("Username and Password required.", 401);
+  }
 
   const data = authorization.split(" ");
 
   if (data[0] !== "Basic") {
-    return res.status(unauthorized).json({ message: "Not Basic Auth Type." });
+    throw new StatusError("Basic Auth Type is required.", 401);
   }
 
   const buff = new Buffer(data[1], "base64");
@@ -13,11 +23,15 @@ const validAuth = async (req, res, next) => {
   const username = text.split(":")[0];
   const password = text.split(":")[1];
 
-  if (password === "" || username === "") {
-    return res.status(unauthorized).json({ message: "Missing Credentials" });
+  if (!password || !username) {
+    throw new StatusError("Missing Credentials.", 401);
   }
 
-  req.credentials = {
+  // sanatize username and password
+  // no spaces for username and password - trim
+  // lowercase for username
+
+  req.user = {
     username,
     password
   };
@@ -25,12 +39,36 @@ const validAuth = async (req, res, next) => {
   next();
 };
 
-const checkUser = async (req, res, next) => {
-  const { username } = req.credentials;
-  const user = await findByUsername(username);
+const authenticateUser = async (req, res, next) => {
+  const { username, password } = req.user;
+  const user = await userModel.findUser({ username });
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    throw new StatusError("Invalid Credentials.", 401);
+  }
+  req.user = user;
+  next();
+};
+
+const generateToken = async (req, res, next) => {
+  const { id, username } = req.user;
+  const payload = {
+    id,
+    username
+  };
+
+  await jwt.sign(payload, secret, (err, token) => {
+    if (err) {
+      // no test for this. need to figure out how to add tests for here
+      return next(new StatusError("Server Error with Token", 500));
+    }
+    req.token = token;
+    next();
+  });
 };
 
 module.exports = {
   validAuth,
-  checkUser
+  authenticateUser,
+  generateToken
 };
